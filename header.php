641784bebcad85219e41ccc7d41cd640f90340be
Tom Flucke <tomflucke@gmail.com>
2014-10-30 14:43:51 +0000

    First commit.

---
+++ header.php
@@ -0,0 +1,8 @@
+<meta name="author" content="Tom Flucke">
+<meta name="copyright" content="">
+<meta name="keywords" content="tom thomas flucke personal">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<link type="text/css" href="styles/tomflucke.css" rel="stylesheet" />

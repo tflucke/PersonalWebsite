e45f178db9e86d1c5238495fa5fd32cfec9552fe
Tom Flucke <tomflucke@gmail.com>
2015-03-24 02:18:07 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<?php require_once 'header.php'; ?>
+		<title>Tom Flucke</title>
+	</head>
+	<body>
+		<div class='white'></div>
+		<header>
+			<h1>
+				Tom Flucke
+			</h1>
+		</header>
+		<div id="content">
+			<div class="pillar">
+				<img />
+			</div>
+			<div class="pillar">
+				
+			</div>
+		</div>
+	</body>
+</html>
